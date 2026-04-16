2398a37496166d31f6c0f52246fea217aa3d0244
Adrian-shift <20231ireinfint0049@ifba.edu.br>
2026-04-16 09:28:25 -0300

    Commit do cliente e servidor

---
+++ cliente.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char voto[10];
+    char buffer[1024] = {0};
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // COLOQUE O IP DO SERVIDOR AQUI
+    inet_pton(AF_INET, "192.168.0.100", &serv_addr.sin_addr);
+
+    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    printf("Digite seu voto (A ou B): ");
+    scanf("%s", voto);
+
+    send(sock, voto, strlen(voto), 0);
+
+    read(sock, buffer, 1024);
+    printf("Resposta do servidor: %s\n", buffer);
+
+    close(sock);
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int votosA = 0;
+int votosB = 0;
+
+int main() {
+    int server_fd, client_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 3);
+
+    printf("Servidor aguardando conexões...\n");
+
+    while(1) {
+        client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+
+        read(client_socket, buffer, 1024);
+        printf("Voto recebido: %s\n", buffer);
+
+        if(strcmp(buffer, "A") == 0) votosA++;
+        else if(strcmp(buffer, "B") == 0) votosB++;
+
+        char resposta[100];
+        sprintf(resposta, "Votos -> A: %d | B: %d", votosA, votosB);
+
+        send(client_socket, resposta, strlen(resposta), 0);
+
+        close(client_socket);
+        memset(buffer, 0, sizeof(buffer));
+    }
+
+    return 0;
+}
